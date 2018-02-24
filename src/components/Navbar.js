32bc7cede057b061e3379458a06745f1cfd48492
Ricky de Laveaga <git@rdela.com>
2018-02-24 00:35:10 -0800

    unbranding

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,20 +10,17 @@ const Navbar = () => (
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
           <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+            <img src={logo} alt="voidcluster" style={{ width: '88px' }} />
           </figure>
         </Link>
       </div>
       <div className="navbar-start">
         <Link className="navbar-item" to="/about">
-          About
-        </Link>
-        <Link className="navbar-item" to="/products">
-          Products
+          about
         </Link>
       </div>
       <div className="navbar-end">
-        <a className="navbar-item" href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate" target="_blank" rel="noopener noreferrer">
+        <a className="navbar-item" href="https://github.com/rdela/voidcluster">
           <span className="icon">
             <img src={github} alt="Github" />
           </span>
