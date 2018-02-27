c1fcc5dd891c9ca57bb5905fdfb188a4603b07cf
Ricky de Laveaga <git@rdela.com>
2018-02-27 13:15:13 -0800

    tag type sizing

--- src/templates/tags.js
+++ src/templates/tags.js
@@ -8,7 +8,7 @@ class TagRoute extends React.Component {
     const postLinks = posts.map(post => (
       <li key={post.node.frontmatter.path}>
         <Link to={post.node.frontmatter.path}>
-          <h2>{post.node.frontmatter.title}</h2>
+          <h2 className="is-size-2">{post.node.frontmatter.title}</h2>
         </Link>
       </li>
     ))
@@ -28,7 +28,7 @@ class TagRoute extends React.Component {
               className="column is-10 is-offset-1"
               style={{ marginBottom: '6rem' }}
             >
-              <h3 className="title is-size-2 is-bold-light">{tagHeader}</h3>
+              <h3 className="title is-size-4 is-bold-light">{tagHeader}</h3>
               <ul className="taglist">{postLinks}</ul>
               <p>
                 <Link to="/tags/">Browse all tags</Link>
