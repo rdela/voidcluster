10f892538f419a4f69f27a04639cc3321cfd8c99
Ricky de Laveaga <git@rdela.com>
2018-02-27 12:38:39 -0800

    require webpackLodashPlugin in gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,6 @@
 const _ = require('lodash')
 const path = require('path')
+const webpackLodashPlugin = require(`lodash-webpack-plugin`)
 
 exports.createPages = ({ boundActionCreators, graphql }) => {
   const { createPage } = boundActionCreators
